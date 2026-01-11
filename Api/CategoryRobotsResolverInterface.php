62b083eb46dd6b3a4e8cbf2dc4dcff26e9f15a2f
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2026-01-11 22:54:32 +0100

    Update CategoryRobotsResolverInterface and update dependencies

--- Api/CategoryRobotsResolverInterface.php
+++ Api/CategoryRobotsResolverInterface.php
@@ -18,10 +18,19 @@ interface CategoryRobotsResolverInterface
     /**
      * Get robots meta tag code for a category page
      *
+     * @deprecated Use getCategoryRobotsDirectives() instead
      * @param Category $category
      * @return int|null Robots code or null if using default
      */
     public function getCategoryRobotsCode(Category $category): ?int;
+
+    /**
+     * Get robots directive array for a category page (v2.0.0+)
+     *
+     * @param Category $category
+     * @return array Directive array (e.g., ['noindex', 'nofollow']) or empty array if using default
+     */
+    public function getCategoryRobotsDirectives(Category $category): array;
 
     /**
      * Check if robots should be applied to products in the category
@@ -34,8 +43,17 @@ interface CategoryRobotsResolverInterface
     /**
      * Get robots meta tag code for products in the category
      *
+     * @deprecated Use getProductRobotsDirectives() instead
      * @param Category $category
      * @return int|null Robots code or null if using default
      */
     public function getProductRobotsCode(Category $category): ?int;
+
+    /**
+     * Get robots directive array for products in the category (v2.0.0+)
+     *
+     * @param Category $category
+     * @return array Directive array (e.g., ['noindex', 'nofollow']) or empty array if using default
+     */
+    public function getProductRobotsDirectives(Category $category): array;
 }
